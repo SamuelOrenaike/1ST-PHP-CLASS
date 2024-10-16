6197f3cdbee3b4ad26235a99cba7c8a6d9134924
Samuel Orenaike <orenaikeibukun@gmail.com>
2024-10-16 11:07:24 +0100

    classwork

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo "I am a FAST learner";
+
+$a = "Boss";
+$q = 4 ;
+echo "<br>";
+echo "$a";
+echo "<br>";
+$x = 5;
+var_dump($x);
+echo "<br>";
+$y = "John";
+var_dump($y);
+echo "<br>";
+$p = 6.9;
+var_dump($p);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump(false);
+
+?>
